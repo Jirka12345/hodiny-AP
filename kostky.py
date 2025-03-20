788bd235fa1b8cf274811d23e6433e35f8fa2783
Jirka12345 <Jiri.Hotek@student.souepl.cz>
2025-03-20 14:23:14 +0100

    Add files via upload

---
+++ kostky.py
@@ -0,0 +1,11 @@
+import random
+counter = 0
+while True:
+    counter += 1
+    kostka1 = random.randint(1,6)
+    kostka2 = random.randint(1,6)
+
+    print(str(kostka1) + " " +str(kostka2))
+    if kostka1 == kostka2:
+        break
+print("Počet hodů nutných na dohrání kostek byl = " + str(counter))
